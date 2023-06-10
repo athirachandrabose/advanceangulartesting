0c966c1727abac8b7af6785c6de9049b057c6007
athirachandrabose <Athira.Chandrabose@ust.com>
2023-06-10 10:35:52 +0530

    first commit

---
+++ src/app/first/first.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FirstComponent } from './first.component';
+
+describe('FirstComponent', () => {
+  let component: FirstComponent;
+  let fixture: ComponentFixture<FirstComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FirstComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FirstComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it(`should have a title 'I love pizza!'`, async(() => {
+    fixture = TestBed.createComponent(FirstComponent);
+    component = fixture.debugElement.componentInstance;
+    expect(component.title).toEqual('I love pizza!');
+  }));
+});
